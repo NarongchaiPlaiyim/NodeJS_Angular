e4ebebf906cfa41820eb159f5ccbb26d0343bffb
Narongchai <N.Plaiyim@gmail.com>
2015-08-12 13:46:17 +0700

    access to db.

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
+//var monogoose = require('monogoose');
+//mongoose.connect('mongodb://127.0.0.1/admin');
 
+var mongojs = require('mongojs');
+var db = mongojs('127.0.0.1/admin', ['itemList']);
 
-app.use(
-    express.static(__dirname + "/views")
-);
+app.use(express.static(__dirname + "/views"));
+app.get("/itemList", function (req, res) {
+
+    db.itemList.find(function (err, docs) {
+        console.log(docs);
+        res.jsonp(docs);
+    });
+    //item = {
+    //    name : 'item',
+    //    price : '100',
+    //    qty : '10'
+    //};
+    //item2 = {
+    //    name : 'item2',
+    //    price : '200',
+    //    qty : '20'
+    //};
+    //item3 = {
+    //    name : 'item3',
+    //    price : '300',
+    //    qty : '30'
+    //};
+    //var itemList = [item, item2, item3];
+    //res.jsonp(itemList);
+});
 
 app.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
--- views/controller/mainController.js
+++ views/controller/mainController.js
@@ -1,3 +1,12 @@
-function initController(){
-    console.log('initController');
-}
+//function InitController(){
+//    console.log('InitController');
+//}
+
+angular.module('mainApp', [])
+    .controller("InitController", function($scope, $http) {
+        $http.get('/itemList')
+            .success(function(response){
+                console.log('/itemList success');
+                $scope.itemList = response;
+        });
+});
