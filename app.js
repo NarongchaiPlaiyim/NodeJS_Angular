c76bab1e31d187fa130cc7d191d42235dabfeeb7
Narongchai <N.Plaiyim@gmail.com>
2015-08-13 17:13:02 +0700

    numberOnly

--- app.js
+++ app.js
@@ -12,41 +12,28 @@ app.get("/itemList", function (req, res) {
         res.json(docs);
     });
 });
-
 app.post("/itemList", function (req, res) {
     db.itemList.insert(req.body, function (err, docs) {
         res.json(docs);
     });
 });
-
 app.delete("/itemList/:id", function (req, res) {
-    var id = req.params.id;
-    db.itemList.remove({_id: mongoJs.ObjectId(id)}, function (err, docs) {
+    db.itemList.remove({_id: mongoJs.ObjectId(req.params.id)}, function (err, docs) {
         res.json(docs);
     });
 });
-
 app.get("/itemList/:id", function (req, res) {
-    var id = req.params.id;
-    db.itemList.findOne({_id: mongoJs.ObjectId(id)}, function (err, docs) {
+    db.itemList.findOne({_id: mongoJs.ObjectId(req.params.id)}, function (err, docs) {
         res.json(docs);
     });
 });
-
 app.put("/itemList/:id", function (req, res) {
-    var id = req.params.id;
-    console.log('update : ');
-    console.log('update : '+id);
-    console.log('update : '+req.body.name);
-    console.log('update : '+req.body.price);
-    console.log('update : '+req.body.qty);
-    db.itemList.findAndModify({query: {_id: mongoJs.ObjectId(id)}, update: {$set: {name:req.body.name,
+    db.itemList.findAndModify({query: {_id: mongoJs.ObjectId(req.params.id)}, update: {$set: {name:req.body.name,
                                                                                    price:req.body.price,
                                                                                    qty:req.body.qty}}, new: true},
         function (err, docs) {
             res.json(docs);
         });
 });
-
 app.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
--- views/controller/mainController.js
+++ views/controller/mainController.js
@@ -3,22 +3,26 @@ angular.module('mainAppModule', ['loginModule'])
         var refresh = function() {
             $http.get('/itemList').success(function(response){
                 $scope.itemList = response;
-                $scope.item = {
-                    name:"", price:"0", qty:"0"
-                };
+                refreshInput();
             });
         };
         refresh();
-
+        var refreshInput = function() {
+            $scope.item = {
+                name:"", price:"0", qty:"0"
+            };
+        };
         $scope.addItem = function(){
             $http.post('/itemList', $scope.item).success(function(response){
                 refresh();
             });
         };
-        $scope.deleteItem = function(id){
-            $http.delete('/itemList/'+id).success(function(response){
-                refresh();
-            });
+        $scope.deleteItem = function(id, name){
+            if (confirm('Are you sure you want to delete '+name+'?')) {
+                $http.delete('/itemList/'+id).success(function(response){
+                    refresh();
+                });
+            }
         };
         $scope.selectItem = function(id){
             $http.get('/itemList/'+id).success(function(response){
@@ -31,16 +35,34 @@ angular.module('mainAppModule', ['loginModule'])
             })
         };
         $scope.clearItem = function(){
-            $scope.item = {
-                name:"", price:"0", qty:"0"
-            };
+            refreshInput();
         };
     })
     .controller("InitController2", function($scope, $http) {
 
-    });
+    })
+    .directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
 
 angular.module('loginModule', [])
     .controller("loginController", function($scope, $http) {
-        $scope.login='123456789';
+        $scope.login='loginModule';
     });
