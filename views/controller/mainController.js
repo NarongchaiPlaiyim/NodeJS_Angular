1e69c4ae33385b64cf5413d72cedb26067d639e5
Narongchai <N.Plaiyim@gmail.com>
2015-08-14 16:42:05 +0700

    Testing MVC

--- views/controller/mainController.js
+++ views/controller/mainController.js
@@ -1,69 +1,57 @@
-angular.module('mainAppModule', ['loginModule', 'utileModule.directives'])
-    //.directive('numbersOnly', ['utileModule.directives'])
-    .controller("InitController", function($scope, $http) {
-        var refresh = function() {
-            $http.get('/itemList').success(function(response){
-                $scope.itemList = response;
-                refreshInput();
-            });
-        };
-        refresh();
-        var refreshInput = function() {
-            $scope.item = {
-                name:"", price:"0", qty:"0"
-            };
-        };
-        $scope.addItem = function(){
-            $http.post('/itemList', $scope.item).success(function(response){
-                refresh();
-            });
-        };
-        $scope.deleteItem = function(id, name){
-            if (confirm('Are you sure you want to delete '+name+'?')) {
-                $http.delete('/itemList/'+id).success(function(response){
-                    refresh();
-                });
-            }
-        };
-        $scope.selectItem = function(id){
-            $http.get('/itemList/'+id).success(function(response){
-                $scope.item = response;
-            })
-        };
-        $scope.updateItem = function(){
-            $http.put('/itemList/'+$scope.item._id, $scope.item).success(function(response){
-                refresh();
-            })
-        };
-        $scope.clearItem = function(){
-            refreshInput();
-        };
-    })
-    .controller("InitController2", function($scope, $http) {
+angular
+    .module('mainAppModule', ['loginModule', 'utileModule.directives'])
+    .controller("InitController", InitController)
+    .controller("InitController2", InitController2);
 
-    })
-    .directive('numbersOnly333333', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, element, attr, ngModelCtrl) {
-            function fromUser(text) {
-                if (text) {
-                    var transformedInput = text.replace(/[^0-9]/g, '');
-
-                    if (transformedInput !== text) {
-                        ngModelCtrl.$setViewValue(transformedInput);
-                        ngModelCtrl.$render();
-                    }
-                    return transformedInput;
-                }
-                return undefined;
-            }
-            ngModelCtrl.$parsers.push(fromUser);
-        }
-    };
-});
-
-angular.module('loginModule', [])
-    .controller("loginController", function($scope, $http) {
+angular
+    .module('loginModule', [])
+    .controller("LoginController", function($scope) {
         $scope.login='loginModule';
     });
+
+InitController.$inject = ['$scope', '$http'];
+function InitController($scope, $http) {
+    var refresh = function() {
+        $http.get('/itemList').success(function(response){
+            $scope.itemList = response;
+            refreshInput();
+        });
+    };
+    refresh();
+    var refreshInput = function() {
+        $scope.item = {
+            name:"", price:0, qty:0
+        };
+    };
+    $scope.addItem = function(){
+        $http.post('/itemList', $scope.item).success(function(response){
+            refresh();
+        });
+    };
+    $scope.deleteItem = function(id, name){
+        if (confirm('Are you sure you want to delete '+name+'?')) {
+            $http.delete('/itemList/'+id).success(function(response){
+                refresh();
+            });
+        }
+    };
+    $scope.selectItem = function(id){
+        $http.get('/itemList/'+id).success(function(response){
+            $scope.item = response;
+        })
+    };
+    $scope.updateItem = function(){
+        $http.put('/itemList/'+$scope.item._id, $scope.item).success(function(response){
+            refresh();
+        })
+    };
+    $scope.clearItem = function(){
+        refreshInput();
+    }
+}
+
+InitController2.$inject = ['$scope'];
+function InitController2($scope) {
+
+}
+
